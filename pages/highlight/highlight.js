15159f1530d86d3770655c76ec5b5f3b4c98d0b6
pengbo <pengbo@drplant.com.cn>
2018-05-02 17:47:27 +0800

    fix

--- pages/highlight/highlight.js
+++ pages/highlight/highlight.js
@@ -1,47 +1,30 @@
-//app.js
-App({
-  onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-    // 登录
-    wx.login({
-      success: res => {
-        console.log(res)
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              console.log("im king")
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data:{
+    storageContent:1111,
+    storageSyncContent: 22222,
+    stname: '',
+    syncstname:''
+  },
+  getdata:function(name){
+    console.log(this.data[name])
   },
   /**
-   * 异步存储
-   */
-  listenerStorageSave: function () {
+    * 异步存储
+    */
+  listenerStorageSave: function (e) {
+    console.log(e.detail.value)
+    var that =this;
     //以键值对的形式存储 传进去的是个对象
     wx.setStorage({
       key: 'key',
-      data: '我是storeage异步存储的信息',
+      data: e.detail.value,
       success: function (res) {
         console.log(res)
+        that.listenerStorageGet()
       }
     })
   },
@@ -111,4 +94,5 @@ App({
   globalData: {
     userInfo: null
   }
+  
 })
--- pages/lowb/lowb.js
+++ pages/lowb/lowb.js
@@ -7,7 +7,17 @@ Page({
   data: {
   
   },
-
+  clickMe: function () {
+    this.setData({ msg: "Hello World" })
+  },
+  jump:function(){
+    wx.navigateTo({
+      url: '../highlight/highlight',
+      success: function(res) {},
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
